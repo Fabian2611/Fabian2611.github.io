7d8cb83beba9bb1c938d3d99172842d6465c95bf
Fabian2611 <fabian.schwarz2010@gmail.com>
2024-10-29 16:43:29 +0100

    Tidier Project list

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ fetch(`https://api.github.com/users/${username}/repos?sort=updated`)
     .then(response => response.json())
     .then(repos => {
         repos.slice(0, 2).forEach(repo => { // Display the latest 5 repositories
-            if (repo.html_url !== "https://github.com/Fabian2611/Fabian2611.github.io"){
-                const repoElement = document.createElement('div');
-                repoElement.innerHTML = `
-                    <h3><a href="${repo.html_url}" target="_blank">${repo.name}</a></h3>
-                    <p><q>${repo.description || 'No description available'}</q></p>
-                `;
-                repoContainer.appendChild(repoElement);
-            }
+            const repoElement = document.createElement('div');
+            repoElement.innerHTML = `
+                <p><b><a href="${repo.html_url}" target="_blank">${repo.name}</a></b></p>
+                <p><q>${repo.description || 'No description.'}</q></p>
+            `;
+            repoContainer.appendChild(repoElement);
         });
     })
     .catch(error => console.error('Error fetching repositories:', error));
