386a0b1ab7e2612ef3a4937e5df79927d2192c22
Fabian2611 <fabian.schwarz2010@gmail.com>
2024-10-30 09:25:50 +0100

    Some styles and rate limit warning

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const username = 'Fabian2611'; // Replace with your GitHub username
 const repoContainer = document.getElementById('repos');
 
-fetch(`https://api.github.com/users/${username}/repos?sort=updated`)
+fetch(`https://api.github.com/users/Fabian2611/repos?sort=updated`)
     .then(response => response.json())
     .then(repos => {
-        repos.slice(0, 2).forEach(repo => { // Display the latest 5 repositories
+        if (Array.isArray(repos)){
+            document.getElementById("no-rates").remove();
+        }
+        repos.slice(0, 2).forEach(repo => {
             const repoElement = document.createElement('div');
             repoElement.innerHTML = `
                 <p><b><a href="${repo.html_url}" target="_blank">${repo.name}</a></b></p>
